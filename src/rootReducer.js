dff92a0f153f7b6fb8057ed57a6083b5c2ec2106
old-stone <mysterious1018@live.jp>
2019-06-02 19:16:06 +0900

    sagaの削除

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
-import idAppReducer from "./modules/idPassApp";
+import idPassAppReducer from "./modules/idPassApp";
 import linkAppReducer from "./modules/linkApp";
 import todoAppReducer from "./modules/todoApp";
 
 export default combineReducers({
   todoAppReducer,
-  idAppReducer,
+  idPassAppReducer,
   linkAppReducer
 });
--- src/store.js
+++ src/store.js
@@ -1,10 +1,4 @@
-import { applyMiddleware, createStore } from "redux";
-
-import createSagaMiddleware from "redux-saga";
+import { createStore } from "redux";
 import rootReducer from "./rootReducer";
-import sagas from "./sagas";
 
-const sagaMiddleware = createSagaMiddleware();
-export default createStore(rootReducer, applyMiddleware(sagaMiddleware));
-
-sagaMiddleware.run(sagas);
+export default createStore(rootReducer);
